862577fadf7d05a166d3a9db20f294bbae246027
zsdnishishui <zsdnishishui@163.com>
2022-10-06 00:39:23 +0800

    Initial commit

---
+++ pc-api/src/main/java/com/zsd/SpringBootPcApplication.java
@@ -0,0 +1,13 @@
+package com.zsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPcApplication.class, args);
+    }
+
+}
