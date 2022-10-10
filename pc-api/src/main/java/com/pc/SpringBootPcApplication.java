d4e9c5ab12f83261d4ed428a992578bb461b9404
zsdnishishui <zsdnishishui@163.com>
2022-10-10 14:24:45 +0800

    修改数据源为druid

--- pc-api/src/main/java/com/pc/SpringBootPcApplication.java
+++ pc-api/src/main/java/com/pc/SpringBootPcApplication.java
@@ -1,10 +1,14 @@
 package com.pc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com"})
+@MapperScan(basePackages = {"com.mapper.sso"})
 @ServletComponentScan(basePackages = {"com.common.filter"})
 public class SpringBootPcApplication {
 
